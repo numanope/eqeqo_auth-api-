b057e61d5acb1e045dd45ba466c6db76d78bd91d
fahedsl <fahedsl@yandex.com>
2025-06-27 08:15:24 -0500

    feat: Add initial routes and handlers
    This commit adds several routes and handlers for user and role
    management. It also includes a placeholder image.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,110 @@
-use httpageboy::{Request, Response, Rt, Server, StatusCode}; // Rt is alias for ResponseType
+use httpageboy::{Request, Response, Rh, Rt, Server, StatusCode};
+use std::collections::HashMap;
+
+fn list_users(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: "Demo: Listar usuarios".as_bytes().to_vec(),
+  }
+}
+
+fn get_user(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: "Demo: Ver perfil de usuario".as_bytes().to_vec(),
+  }
+}
+
+fn create_user(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: "Demo: Crear usuario".as_bytes().to_vec(),
+  }
+}
+
+fn update_user(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: "Demo: Editar usuario".as_bytes().to_vec(),
+  }
+}
+
+fn delete_user(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: Vec::new(),
+  }
+}
+
+fn list_roles(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: "Demo: Listar roles".as_bytes().to_vec(),
+  }
+}
+
+fn create_role(_req: &Request) -> Response {
+  Response {
+    status: StatusCode::Ok.to_string(),
+    content_type: String::new(),
+    content: "Demo: Crear rol".as_bytes().to_vec(),
+  }
+}
 
 fn main() {
   let serving_url: &str = "127.0.0.1:7878";
   let threads_number: u8 = 10;
-  let mut server = Server::new(serving_url, threads_number, None).unwrap();
-  server.add_route("/", Rt::GET, demo_get);
-  server.add_route("/", Rt::POST, demo_post);
+
+  let mut routes: HashMap<(Rt, String), Rh> = HashMap::new();
+
+  routes.insert(
+    (Rt::GET, "/users".to_string()),
+    Rh {
+      handler: list_users,
+    },
+  );
+  routes.insert(
+    (Rt::GET, "/users/{id}".to_string()),
+    Rh { handler: get_user },
+  );
+  routes.insert(
+    (Rt::POST, "/users".to_string()),
+    Rh {
+      handler: create_user,
+    },
+  );
+  routes.insert(
+    (Rt::PUT, "/users/{id}".to_string()),
+    Rh {
+      handler: update_user,
+    },
+  );
+  routes.insert(
+    (Rt::DELETE, "/users/{id}".to_string()),
+    Rh {
+      handler: delete_user,
+    },
+  );
+  routes.insert(
+    (Rt::GET, "/roles".to_string()),
+    Rh {
+      handler: list_roles,
+    },
+  );
+  routes.insert(
+    (Rt::POST, "/roles".to_string()),
+    Rh {
+      handler: create_role,
+    },
+  );
+
+  // 3. Paso ese HashMap al servidor
+  let server = Server::new(serving_url, threads_number, Some(routes)).unwrap();
   server.run();
-}
-
-fn demo_get(_request: &Request) -> Response {
-  return Response {
-    status: StatusCode::Ok.to_string(),
-    content_type: String::new(),
-    content: "get".as_bytes().to_vec(),
-  };
-}
-
-fn demo_post(_request: &Request) -> Response {
-  return Response {
-    status: StatusCode::Ok.to_string(),
-    content_type: String::new(),
-    content: "post".as_bytes().to_vec(),
-  };
 }
